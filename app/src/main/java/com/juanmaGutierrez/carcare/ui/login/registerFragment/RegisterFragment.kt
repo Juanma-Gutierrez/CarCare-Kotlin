347617436012064723c77bdac93045c082fe2df7
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2024-03-24 14:52:56 +0100

    Add error messages in register translated

--- app/src/main/java/com/juanmaGutierrez/carcare/ui/login/registerFragment/RegisterFragment.kt
+++ app/src/main/java/com/juanmaGutierrez/carcare/ui/login/registerFragment/RegisterFragment.kt
@@ -5,83 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.juanmaGutierrez.carcare.databinding.FragmentRegisterBinding
-import com.juanmaGutierrez.carcare.model.User
 import com.juanmaGutierrez.carcare.service.showSnackBar
 
 class RegisterFragment : Fragment() {
-    private val viewModel: RegisterViewModel by viewModels()
+    private lateinit var viewModel: RegisterViewModel
     private lateinit var binding: FragmentRegisterBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+/*    override fun onCreate(savedInstanceState: Bundle?) {
         binding = FragmentRegisterBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-    }
+    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        binding.reBtRegister.setOnClickListener { register() }
-        binding.reBtLogin.setOnClickListener { goLogin() }
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+
+        binding.reBtRegister.setOnClickListener { viewModel.register(binding) }
+        binding.reBtLogin.setOnClickListener { fragmentManager?.popBackStack() }
         return binding.root
     }
 
-    private fun register() {
-        val user: User = readUser()
-        if (!validUserData(user)) return
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[RegisterViewModel::class.java]
 
-    private fun validUserData(user: User): Boolean {
-        print(user)
-        if (someFieldEmpty(user)) {
-            showSnackBar("No puedes dejar campos vacíos", requireView())
-            return false
+        viewModel.showSnackbarEvent.observe(viewLifecycleOwner) { message ->
+            showSnackBar(message, view)
         }
-        if (!passwordIsValid(user)) return false
-        print("usuario válido")
-        return true
-    }
-
-    private fun passwordIsValid(user: User): Boolean {
-        if (!user.password.equals(user.repeatPassword)) {
-            showSnackBar("Las contraseñas no coinciden", requireView())
-            return false
-        }
-        if (!validatePasswordCharacters(user.password)) {
-            showSnackBar("La contraseña es demasiado débil", requireView())
-            return false
-        }
-        return true
-    }
-
-    private fun validatePasswordCharacters(password: String): Boolean {
-        val pattern = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")
-        return pattern.matches(password)
-    }
-
-
-    private fun someFieldEmpty(user: User): Boolean {
-        return (user.name.isEmpty() or
-                user.surname.isEmpty() or
-                user.username.isEmpty() or
-                user.email.isEmpty() or
-                user.password.isEmpty())
-    }
-
-    private fun readUser(): User {
-        return User(
-            name = binding.reItName.text.toString(),
-            surname = binding.reItSurname.text.toString(),
-            username = binding.reItUsername.text.toString(),
-            email = binding.reItEmail.text.toString(),
-            password = binding.reItPassword.text.toString(),
-            repeatPassword = binding.reItRepeatPassword.text.toString()
-        )
-    }
-
-    private fun goLogin() {
-        print("ir a login")
-        fragmentManager?.popBackStack()
     }
 }
--- app/src/main/java/com/juanmaGutierrez/carcare/ui/login/registerFragment/RegisterViewModel.kt
+++ app/src/main/java/com/juanmaGutierrez/carcare/ui/login/registerFragment/RegisterViewModel.kt
@@ -1,6 +1,71 @@
 package com.juanmaGutierrez.carcare.ui.login.registerFragment
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.juanmaGutierrez.carcare.R
+import com.juanmaGutierrez.carcare.databinding.FragmentRegisterBinding
+import com.juanmaGutierrez.carcare.model.User
 
-class RegisterViewModel : ViewModel() {
+class RegisterViewModel(application: Application) : AndroidViewModel(application) {
+    private val _showSnackbarEvent = MutableLiveData<String>()
+    val showSnackbarEvent: LiveData<String>
+        get() = _showSnackbarEvent
+
+    fun register(binding: FragmentRegisterBinding) {
+        val user: User = readUser(binding)
+        if (!validUserData(user)) return
+    }
+
+    private fun validUserData(user: User): Boolean {
+        print(user)
+        if (someFieldEmpty(user)) {
+            _showSnackbarEvent.value =
+                getApplication<Application>().getString(R.string.error_emptyFields)
+            return false
+        }
+        if (!passwordIsValid(user)) return false
+        return true
+    }
+
+    private fun passwordIsValid(user: User): Boolean {
+        if (user.password != user.repeatPassword) {
+            _showSnackbarEvent.value =
+                getApplication<Application>().getString(R.string.error_passwordNotMatch)
+            return false
+        }
+        if (!validatePasswordCharacters(user.password)) {
+            _showSnackbarEvent.value =
+                getApplication<Application>().getString(R.string.error_passwordWeak)
+            return false
+        }
+        return true
+    }
+
+    private fun validatePasswordCharacters(password: String): Boolean {
+        val pattern = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")
+        return pattern.matches(password)
+    }
+
+
+    private fun someFieldEmpty(user: User): Boolean {
+        return (user.name.isEmpty() or
+                user.surname.isEmpty() or
+                user.username.isEmpty() or
+                user.email.isEmpty() or
+                user.password.isEmpty())
+    }
+
+    fun readUser(binding: FragmentRegisterBinding): User {
+        return User(
+            name = binding.reItName.text.toString(),
+            surname = binding.reItSurname.text.toString(),
+            username = binding.reItUsername.text.toString(),
+            email = binding.reItEmail.text.toString(),
+            password = binding.reItPassword.text.toString(),
+            repeatPassword = binding.reItRepeatPassword.text.toString()
+        )
+    }
+
 }
